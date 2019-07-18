fcfff57f0018fbb1644bbed408024d6afcc50dae
realgreeshma <greeshmahere@gmail.com>
2019-07-18 06:58:08 -0500

    Created Test package and started class to test with annotations

---
+++ src/test/java/com/qa/halfpricebooks/tests/LoginPageTest.java
@@ -0,0 +1,28 @@
+package com.qa.halfpricebooks.tests;
+
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+import com.qa.halfpricebooks.base.BasePage;
+import com.qa.halfpricebooks.pages.LoginPage;
+
+public class LoginPageTest {
+	WebDriver driver;
+	Properties property;
+	BasePage basePage;
+	LoginPage loginPage;
+	
+	@BeforeMethod
+	public void setUp()
+	{
+		basePage = new BasePage();
+		property = basePage.properties_initialisation();//if it returns properties store in Properties :: Mapping
+		driver = basePage.driver_initialisation(property);//using property variable to initialize driver method
+		
+		loginPage = new LoginPage(driver);
+		}
+
+	
+}
