9020e50998b9c2cc0b7e0314825a2dc3a26ed811
realgreeshma <greeshmahere@gmail.com>
2019-07-18 05:13:38 -0500

    Added Initializing driver method in BasePage

--- src/main/java/com/qa/halfpricebooks/base/BasePage.java
+++ src/main/java/com/qa/halfpricebooks/base/BasePage.java
@@ -1,5 +1,39 @@
 package com.qa.halfpricebooks.base;
 
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BasePage {
+	
+	WebDriver driver;
+	Properties prop;
+	
+	public WebDriver driver_initialisation(Properties prop)
+	{
+		String browserName = prop.getProperty("browser");
+		if(browserName.equalsIgnoreCase("chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+		else if (browserName.equalsIgnoreCase("FireFox")) {
+
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+		else {
+			System.out.println("Please enter a valid Browser Name as mentioned in properties file \n"+browserName +"is not VALID");
+		}
+		
+		driver.manage().deleteAllCookies();
+		driver.manage().window().fullscreen();
+		
+		return driver;
+	}
 
 }
