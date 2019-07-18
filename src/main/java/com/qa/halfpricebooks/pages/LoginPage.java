e889e0d7ba2193a48982f80351b9fe013a48ebc4
realgreeshma <greeshmahere@gmail.com>
2019-07-18 06:55:35 -0500

    Created Utility methods to getTitle and verifyLink and LoggingIn

--- src/main/java/com/qa/halfpricebooks/pages/LoginPage.java
+++ src/main/java/com/qa/halfpricebooks/pages/LoginPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 import com.qa.halfpricebooks.base.BasePage;
 
 public class LoginPage extends BasePage {
-	//Defining Locators with the help of PageFactory -- Page Objects or WebElements
+	//Defining Locators with the help of PageFactory -- Page Objects or WebElements --- 1st step
 	
 	@FindBy(id="spree_user_email")
 	WebElement emailAddress;
@@ -22,12 +22,38 @@ public class LoginPage extends BasePage {
 	@FindBy(linkText="click here")
 	WebElement returningGuest;
 	
-	//Create Constructor of this class and intialize page objects
+	//Create Constructor of this class and intialize page objects--2nd step
 	
 	public LoginPage(WebDriver driver)
 	{
 		this.driver = driver;
 		PageFactory.initElements(driver, this); // or PageFactory.initElements(driver, LoginPage.class);
 	}
-
+//3rd step -- Page Actions
+	/**
+	 * This method is to get LoginPage Title
+	 * @return
+	 */
+	public String getLoginPageTitle() 
+	   {
+		return driver.getTitle();
+		}
+	/**
+	 * This method is used to check for the below link
+	 * @return
+	 */
+	public boolean verifyReturnGuestLink() 
+		{
+		return returningGuest.isEnabled();
+		}
+	/**
+	 * This method is used to login with credentials
+	 * @param userName
+	 * @param pwd
+	 */
+	public void login_credentials(String userName,String pwd) {
+		emailAddress.sendKeys(userName);
+		password.sendKeys(pwd);
+		logIn.click();
+		}
 }
