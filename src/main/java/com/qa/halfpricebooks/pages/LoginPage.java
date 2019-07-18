b78c66bffdf68c590a384cfd5a92e6f5765f1a07
realgreeshma <greeshmahere@gmail.com>
2019-07-18 06:06:07 -0500

    Creating a Constructor inside LoginPage with driver as a parameter to initialise the pageobjects

--- src/main/java/com/qa/halfpricebooks/pages/LoginPage.java
+++ src/main/java/com/qa/halfpricebooks/pages/LoginPage.java
@@ -1,12 +1,14 @@
 package com.qa.halfpricebooks.pages;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import com.qa.halfpricebooks.base.BasePage;
 
 public class LoginPage extends BasePage {
-	//Defining Locators with the help of PageFactory
+	//Defining Locators with the help of PageFactory -- Page Objects or WebElements
 	
 	@FindBy(id="spree_user_email")
 	WebElement emailAddress;
@@ -20,6 +22,12 @@ public class LoginPage extends BasePage {
 	@FindBy(linkText="click here")
 	WebElement returningGuest;
 	
+	//Create Constructor of this class and intialize page objects
 	
+	public LoginPage(WebDriver driver)
+	{
+		this.driver = driver;
+		PageFactory.initElements(driver, this); // or PageFactory.initElements(driver, LoginPage.class);
+	}
 
 }
