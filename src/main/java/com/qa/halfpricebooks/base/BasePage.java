84a63cbe52c45978dbee15f7f017348f7970afff
realgreeshma <greeshmahere@gmail.com>
2019-07-18 04:55:48 -0500

    Initial Commit with Selenium,TestNG,bonnigracia dependencies and made project skeleton with configuration,base,pages packages

---
+++ src/main/java/com/qa/halfpricebooks/base/BasePage.java
@@ -0,0 +1,5 @@
+package com.qa.halfpricebooks.base;
+
+public class BasePage {
+
+}
---
+++ src/main/java/com/qa/halfpricebooks/pages/LoginPage.java
@@ -0,0 +1,7 @@
+package com.qa.halfpricebooks.pages;
+
+import com.qa.halfpricebooks.base.BasePage;
+
+public class LoginPage extends BasePage {
+
+}
