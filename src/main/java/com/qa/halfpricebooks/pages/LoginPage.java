f35717de4504d68a8a90c1b423d3d656ccc558a5
realgreeshma <greeshmahere@gmail.com>
2019-07-18 05:50:28 -0500

    Created Locators in LoginPage with the help of PageFactory

--- src/main/java/com/qa/halfpricebooks/pages/LoginPage.java
+++ src/main/java/com/qa/halfpricebooks/pages/LoginPage.java
@@ -1,7 +1,25 @@
 package com.qa.halfpricebooks.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 import com.qa.halfpricebooks.base.BasePage;
 
 public class LoginPage extends BasePage {
+	//Defining Locators with the help of PageFactory
+	
+	@FindBy(id="spree_user_email")
+	WebElement emailAddress;
+	
+	@FindBy(id="spree_user_password")
+	WebElement password;
+	
+	@FindBy(id="login_button")
+	WebElement logIn;
+	
+	@FindBy(linkText="click here")
+	WebElement returningGuest;
+	
+	
 
 }
