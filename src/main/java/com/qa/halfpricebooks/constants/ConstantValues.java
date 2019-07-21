c8c39d94d1414877dcbb328cc1add80da1cb11ee
realgreeshma <greeshmahere@gmail.com>
2019-07-20 19:39:03 -0500

    Added Sales Link value as a constant

--- src/main/java/com/qa/halfpricebooks/constants/ConstantValues.java
+++ src/main/java/com/qa/halfpricebooks/constants/ConstantValues.java
@@ -1,5 +1,8 @@
 package com.qa.halfpricebooks.constants;
 
 public class ConstantValues {
-	public static final String LOGIN_PAGE_TITLE = "Log in to Your Account,Create Account|Half Price Books";
+	public static final String LOGIN_PAGE_TITLE = "Log in to Your Account, Create Account | Half Price Books";
+
+	public static final String SALE_PAGE_URL = "https://hpb.com/quirky-feed/clearance-sale";
+
 }
