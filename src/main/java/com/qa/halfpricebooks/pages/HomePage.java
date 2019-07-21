100ac9dbfeaceea58f6c449f002834b5de012384
realgreeshma <greeshmahere@gmail.com>
2019-07-20 19:59:46 -0500

    Added Page objects , Constructor to HomePage class

--- src/main/java/com/qa/halfpricebooks/pages/HomePage.java
+++ src/main/java/com/qa/halfpricebooks/pages/HomePage.java
@@ -1,7 +1,45 @@
 package com.qa.halfpricebooks.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import com.qa.halfpricebooks.base.BasePage;
 
 public class HomePage extends BasePage {
-
+	
+	  //Page Objects
+	  
+	  @FindBy(linkText ="/stores")
+	  WebElement storeLocator;
+	  
+	  @FindBy(xpath="//i[@class='icons-cart-white mll']")
+	  WebElement shoppingCart;
+	  
+	  @FindBy(id = "welcome-back-name") 
+	  WebElement loggedInName;
+	  
+	  @FindBy(xpath="//a[@class='logo icons-header-logo']")
+	  WebElement HeaderLogo;
+	  
+	  @FindBy(css="body > div.page-wrap > div.secondary-header > div > div.small-7.large-6.columns > div > form > button")
+	  WebElement searchBox;
+	  
+	  @FindBy(name="keywords")
+	  WebElement searchQuerry;
+	  
+	  //Creating a constructor again same as LoginPAge
+	  public HomePage(WebDriver driver)
+	  {
+		  this.driver = driver;
+		  PageFactory.initElements(driver, this);
+	  }
+	  
+	  //Page Actions
+	   public void checkForStroreLocations()
+	  {
+		  
+	  }
+	 
 }
