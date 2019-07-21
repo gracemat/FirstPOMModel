66d6e5ead29e3df0881af6f5b38891adb9fdb0b6
realgreeshma <greeshmahere@gmail.com>
2019-07-20 21:54:25 -0500

    Added Find a store page title

--- src/main/java/com/qa/halfpricebooks/constants/ConstantValues.java
+++ src/main/java/com/qa/halfpricebooks/constants/ConstantValues.java
@@ -4,5 +4,7 @@ public class ConstantValues {
 	public static final String LOGIN_PAGE_TITLE = "Log in to Your Account, Create Account | Half Price Books";
 
 	public static final String SALE_PAGE_URL = "https://hpb.com/quirky-feed/clearance-sale";
+	
+	public static final String STORE_LOCATIONS_PAGE_TITLE = "Our Stores, Find a Store | Half Price Books";
 
 }
